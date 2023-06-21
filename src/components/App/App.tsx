7fb41e63c75bf865dc152ef0121048497a82b45a
Matheus Dela Rosa <delarosamonteiro@gmail.com>
2023-06-21 13:51:12 +0200

    Minimum adjustments

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <h1>Batata</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima vitae,
+        ut temporibus esse quam repudiandae velit, veniam omnis beatae, placeat
+        voluptatum inventore modi eius? Quasi ut soluta et quos fuga!
+      </p>
+      <ul>
+        <li>batata</li>
+        <li>batataa</li>
+        <li>bataaaa</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
