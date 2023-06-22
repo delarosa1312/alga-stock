2e5ba2ad51b27ddc1f9697c3c22def79f12b6351
Matheus Dela Rosa <delarosamonteiro@gmail.com>
2023-06-22 12:03:35 +0200

    add button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Header from "../Header";
+import Button from "../Button";
 import "./App.css";
 
+function TestComponent () {
+  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
+}
+
 function App() {
-  return <div className="App">
+  return (
+    <div className="App">
       <Header title="AlgaStock" />
-  </div>
+
+      <div className="Container">
+        <Button
+          onClick={() => window.alert("Alert")}
+          appendIcon={<TestComponent />}
+        >
+          Alert 
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
