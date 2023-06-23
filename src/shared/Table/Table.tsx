6fd14ee2b8907b14b810ab9fc5683ed3cd170969
Matheus Dela Rosa <delarosamonteiro@gmail.com>
2023-06-23 15:08:05 +0200

    add sass package + add table style

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,28 +1,60 @@
-import React from 'react'
-// import './Table.scss'
+import React from "react";
+import "./Table.scss";
 
 const Table = () => {
-  return <table>
-    <thead>
-      <tr>
-        <th>Product</th>
-        <th>Price</th>
-        <th>Stock</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Cookie</td>
-        <td>$1.25</td>
-        <td>23</td>
-      </tr>
-      <tr>
-        <td>Milk</td>
-        <td>$0.93</td>
-        <td>10</td>
-      </tr>
-    </tbody>
-  </table>
-}
+  return (
+    <table className="AppTable">
+      <thead>
+        <tr>
+          <th className="left">Product</th>
+          <th className="center">Price</th>
+          <th className="right">Stock</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="left">Cookie</td>
+          <td>$1.25</td>
+          <td className="right">23</td>
+        </tr>
+        <tr>
+          <td className="left">Milk</td>
+          <td>$0.93</td>
+          <td className="right">10</td>
+        </tr>
+        <tr>
+          <td className="left">Cookie</td>
+          <td>$1.25</td>
+          <td className="right">23</td>
+        </tr>
+        <tr>
+          <td className="left">Milk</td>
+          <td>$0.93</td>
+          <td className="right">10</td>
+        </tr>
+        <tr>
+          <td className="left">Cookie</td>
+          <td>$1.25</td>
+          <td className="right">23</td>
+        </tr>
+        <tr>
+          <td className="left">Milk</td>
+          <td>$0.93</td>
+          <td className="right">10</td>
+        </tr>
+        <tr>
+          <td className="left">Cookie</td>
+          <td>$1.25</td>
+          <td className="right">23</td>
+        </tr>
+        <tr>
+          <td className="last-left">Milk</td>
+          <td>$0.93</td>
+          <td className="last-right">10</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
-export default Table
+export default Table;
