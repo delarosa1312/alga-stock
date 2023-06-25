35b3fc442c0aec668b2f0b48b58dc7a45a062d34
Matheus Dela Rosa <delarosamonteiro@gmail.com>
2023-06-25 14:20:44 +0200

    add product-form component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,9 +4,7 @@ import Header from "../Header";
 import Container from "../../shared/Container";
 import Table, { TableHeader } from "../../shared/Table";
 import Products from "../../shared/Table/Table.mockdata";
-import Form from "../../shared/Form";
-import Input from "../../shared/Input";
-import Button from "../../shared/Button";
+import ProductForm from "../Products/ProductForm";
 
 const headers: TableHeader[] = [
   {
@@ -34,19 +32,8 @@ function App() {
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
+        <ProductForm />
         <Table headers={headers} data={Products} />
-        <Form title="Product form" onSubmit={console.log}>
-          <Input label="Name" placeholder="E.g.: Cookie" />
-          <Input
-            label="Price"
-            type="number"
-            step="0.01"
-            min="0"
-            placeholder="E.g.: 1.25"
-          />
-          <Input label="Stock" type="number" min="0" placeholder="E.g.: 15" />
-          <Button>Submit</Button>
-        </Form>
       </Container>
     </div>
   );
